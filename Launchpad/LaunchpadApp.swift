9545679014edea3d54926bd44dcdf971b9fa4992
AWildTeriyaki <terrancexia321@gmail.com>
2026-01-28 08:35:36 -0500

    Initial Commit

---
+++ Launchpad/LaunchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchpadApp.swift
+//  Launchpad
+//
+//  Created by Terrance Xia (student LM) on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct LaunchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaunchpadTests/LaunchpadTests.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchpadTests.swift
+//  LaunchpadTests
+//
+//  Created by Terrance Xia (student LM) on 1/28/26.
+//
+
+import Testing
+@testable import Launchpad
+
+struct LaunchpadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
